e1ff9b4a2f28b406956c5e2fd94d71ac56448a36
Nejc <nejc.znidar@gmail.com>
2022-12-12 23:50:43 +0100

    Graphical updates to appreciation figure
    Graphical updates to appreciation figure

--- Financial_modeling/appreciation_video/coin_appreciation.R
+++ Financial_modeling/appreciation_video/coin_appreciation.R
@@ -32,15 +32,28 @@ plotted_data$Dollar <- 1
 
 plotted_data <- plotted_data[!(names(plotted_data) %in% c("Date"))]
 
+
 plotted_data <- melt(plotted_data,id.vars=c("date"),measured.vars=c("EUR","Dollar","G-coin"))
 
-
-
-p <- ggplot(data = plotted_data, mapping = aes(x = date, y = value, colour = variable)) +
+p <- ggplot(data = plotted_data, mapping = aes(x = date, y = value,group = variable, colour = variable)) +
   geom_line() +
-  labs(x = "Date", y = "USD value", title = "Appreciation of G-coin and Euro versus the dollar over time") +
+  labs(x = "Date", y = "USD value", 
+       title = "Appreciation of G-coin and Euro versus the dollar over time") +
   geom_text(aes(label = variable), vjust = 0) +
+  scale_color_manual(values=c("#D1AC00", "#F47D20","#0C1618")) +
+  theme(plot.background = element_rect(fill = "#53625C"), 
+        panel.background = element_rect(fill = "#53625C", colour="#53625C"),
+        plot.title= element_text(size=15,color="#FFFFFF",face="bold"),
+        axis.text.x=element_text(colour="#FFFFFF"),
+        axis.title.y = element_text(colour="#FFFFFF"),
+        axis.title.x = element_text(colour="#FFFFFF"),
+        legend.background = element_rect(fill = "#53625C"),
+        legend.title = element_text(size=15,color="#FFFFFF",face="bold"),
+        legend.text = element_text(colour="#FFFFFF")) +
   transition_reveal(along = date) + view_follow()
+
+p <- p + guides(colour = guide_legend("Legend"))
+
 p
 
 animate(p,nframes = 800, fps = 10)
